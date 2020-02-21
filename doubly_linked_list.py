1fb6cc8b938974a5dfd06993d1a0e77e8e1fadbc
Tom <tomtarpeydev@gmail.com>
2020-02-21 18:18:20 +0000

    first pass node and dll structure

---
+++ doubly_linked_list.py
@@ -0,0 +1,70 @@
+""" 
+A Simple DLL 
+"""
+
+# Node Class
+# - value
+# - prev
+# - next
+class Node:
+    # constructor
+    def __init__(self, value, prev=None, next=None):
+        pass
+
+    # insert after
+    def insert_after(self, value):
+        pass
+
+    # insert before
+    def insert_before(self, value):
+        pass
+
+    # delete
+    def delete(self):
+        pass
+
+
+# DoublyLinkedList Class
+# - head
+# - tail
+# - length
+class DoublyLinkedList:
+    # constructor
+    def __init__(self, node=None):
+        pass
+
+    # get the length
+    def __len__(self):
+        pass
+
+    # add to head
+    def add_to_head(self, value):
+        pass
+
+    # add to tail
+    def add_to_tail(self, value):
+        pass
+
+    # get max
+    def get_max(self):
+        pass
+
+    # delete
+    def delete(self, node):
+        pass
+
+    # remove from head
+    def remove_frome_head(self):
+        pass
+
+    # remove from tail
+    def remove_from_tail(self):
+        pass
+
+    # move to front
+    def move_to_front(self, node):
+        pass
+
+    # move to end
+    def move_to_end(self, node):
+        pass
